0a9d84fdbc325d2147406d807234b49f02649abe
waliglek <104226250+waliglek@users.noreply.github.com>
2022-04-23 14:39:19 +0200

    Add files via upload

---
+++ flask/fbapp/classes/Utilisateur.py
@@ -0,0 +1,28 @@
+
+class Utilisateur :
+
+    def __init__ (self, email="", nom="", motdepasse=""):
+
+        self.__email=email
+        self.__nom = nom
+        self.__motdepasse= motdepasse
+
+
+
+    def getNomUtilisateur(self):
+        return self.__nom
+'''
+    def addSeance(seance=Seance, day=Day):
+        return 0;
+
+    def removeSeance(seance=Seance, day=Day):
+        return 0;
+    
+    def addExo(exo=Exo, day=Day):
+        return 0;
+
+    def removeExo(exo=Exo, day=Day):
+        return 0;
+'''
+
+
